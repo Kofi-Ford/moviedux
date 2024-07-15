b0eebabb49c87c0babb90396a31be8f9ff85ce2c
Kofi_Ford <fordkofi@gmail.com>
2024-07-15 14:01:54 -0500

    Created Error Handling for incorrect images

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,9 +3,14 @@ import '../styles.css';
 
 export default function MoviesCard({movie}) {
 
+    const handleError = (e) => {
+        e.target.src = "images/default.jpg"
+    }
+
+
     return (
       <div key={movie.id} className="movie-card">
-        <img src={`images/${movie.image}`} alt={movie.title} />
+        <img src={`images/${movie.image}`} alt={movie.title} onError={handleError} />
         <div>
           <h3 className="movie-card-title"> {movie.title} </h3>
           <p className="movie-card-genre"> {movie.genre}</p>
